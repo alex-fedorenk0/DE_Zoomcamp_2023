da0ec05d34ba2cecd9f4a7d75538c24ca11c2419
alex-fedorenk0 <alex.fedorenko@gmail.com>
2023-01-29 13:18:24 +0200

    homework week 1

---
+++ week_1/ingest_data.py
@@ -0,0 +1,54 @@
+import argparse
+import pandas as pd
+from sqlalchemy import create_engine
+from time import time
+import os
+
+def main(params):
+    user = params.user
+    password = params.password
+    host = params.host 
+    port = params.port 
+    db = params.db
+    table_name = params.table_name
+    url = params.url
+
+    if url.endswith('.csv.gz'):
+        csv_name = 'output.csv.gz'
+    else:
+        csv_name = 'output.csv'
+
+    os.system(f"wget {url} -O {csv_name}")
+
+    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
+
+    df_iter = pd.read_csv(csv_name, iterator=True, chunksize=100_000)
+
+    df = next(df_iter)
+    # df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+    # df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+    df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
+    df.to_sql(name=table_name, con=engine, if_exists='append')
+
+    for df in df_iter:
+        t_start = time()
+        # df.lpep_dropoff_datetime = pd.to_datetime(df.lpep_dropoff_datetime)
+        # df.lpep_pickup_datetime = pd.to_datetime(df.lpep_pickup_datetime)
+        df.to_sql(name=table_name, con=engine, if_exists='append')
+        t_end = time()
+
+        print(f'inserted chunk, time {t_end-t_start:4f} seconds')
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Ingest CSV data to Postgres')
+
+    parser.add_argument('--user', required=True, help='user name for postgres')
+    parser.add_argument('--password', required=True, help='password for postgres')
+    parser.add_argument('--host', required=True, help='host for postgres')
+    parser.add_argument('--port', required=True, help='port for postgres')
+    parser.add_argument('--db', required=True, help='database name for postgres')
+    parser.add_argument('--table-name', required=True, help='name of the table where we will write th results ')
+    parser.add_argument('--url', required=True, help='url of the csv file')
+
+    main(parser.parse_args())
